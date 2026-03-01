f3fe6d143a74aebb01aea1d959d0039da0a8a08a
Łukasz K <133589278+kocyk908@users.noreply.github.com>
2026-03-01 15:19:41 +0000

    First "Hello world!" Java program

---
+++ src/main/java/com/example/main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class Main
+{
+    // może zawierać elementy typu: funkcje i dane
+    public static void main(String[] args) // musi być, główna-podstawowa funkcja - wywołana przy uruchomieniu programu
+    {
+        System.out.println("Hello world!");
+    }
+}
