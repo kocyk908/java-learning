44a38602c232df845ed79af615cba696e725f0eb
Łukasz K <133589278+kocyk908@users.noreply.github.com>
2026-03-01 15:45:52 +0000

    Ex.2 - scanner (odpowiednik input) i pętle (to samo co C)

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,11 +1,31 @@
 package com.example;
 
+import java.util.Scanner;
+
 public class Main
 {
     // może zawierać elementy typu: funkcje i dane
     public static void main(String[] args) // musi być, główna-podstawowa funkcja - wywołana przy uruchomieniu programu
     {
-        System.out.println("Hello world!");
-        System.out.println("Welcome to Maven!");
+        // System.out.println("Hello world!");
+        // System.out.println("Welcome to Maven!");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true)
+        {
+            System.out.print("Podaj swoje imię (lub wpisz 'exit' aby wyjść): ");
+            String name = scanner.nextLine();
+            
+            if (name.equals("exit")) // equals - porównanie zawartości stringa, == - porównanie referencji
+            {
+                System.out.println("Do widzenia!");
+                break;
+            }
+            
+            System.out.println("Hello " + name + "!");
+        }
+        
+        scanner.close();
     }
 }
